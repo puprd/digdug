a0a5fe08ba90b06140420bf4337a8f1d1c2870ab
Sai Hsu <hsusai16@gmail.com>
2026-07-20 16:28:42 -0400

    commit2

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@
 require 'src.Dependencies'
 local background
 local digdug
+
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
@@ -18,9 +19,16 @@ function love.load()
     digdug = love.graphics.newImage("digdug.png")
     hole = love.graphics.newImage("blockhole.png")
 end
+local function printgrid()
+    for i = 1, 30, 1 do
+        for j = 1, 30, 1 do
+            love.graphics.print(GRID[i][j], i  * 10, j * 10) -- Set the default value
+        end
+    end
+end
 -- function love.resize(w, h)
 --     push.resize(w, h)
--- end
+-- en
 
 -- love.graphics.draw(gTextures['background'], 0, 0)
 -- local backgroundWidth = gTextures['background']:getWidth()
@@ -36,23 +44,30 @@ end
 function love.draw()
     push:start()
 
-    local sx = VIRTUAL_WIDTH / background:getWidth() 
-    local sy = VIRTUAL_HEIGHT / background:getHeight() 
-    
+    local sx = VIRTUAL_WIDTH / background:getWidth()
+    local sy = VIRTUAL_HEIGHT / background:getHeight()
+
     love.graphics.draw(background, 0, 0, 0, sx, sy)
-     local sx = 0.2 --VIRTUAL_WIDTH / 1500--2183.33
-    local sy = 0.2--VIRTUAL_HEIGHT / 1625--2408.333333333333333333333333333333333333333333333333333333333
-    --each block = 50
-    --12 x 12 block map
+    local sx = 0.2 -- VIRTUAL_WIDTH / 1500--2183.33
+    local sy = 0.2 -- VIRTUAL_HEIGHT / 1625--2408.333333333333333333333333333333333333333333333333333333333
+    -- each block = 50
+    -- 12 x 12 block map
     local digdugwidth = digdug:getWidth() * sx / 2
     local digdugheight = digdug:getHeight() * sx / 2
-    --love.graphics.print(digdug:getWidth() * sx, 0, 0)
-    local holeS = 25 / 57--0.4386
-    love.graphics.print(holeS)
-    for  i = 2.5, 25, 1 do
+    -- love.graphics.print(digdug:getWidth() * sx, 0, 0)
+    local holeS = 25 / 57 -- 0.4386
+    --love.graphics.print(holeS)
+    printgrid()
+    for i = 2.5, 25, 1 do
         love.graphics.draw(hole, 0, i * 20, 0, holeS, holeS)
     end
-    --hi
+    -- hi
     love.graphics.draw(digdug, VIRTUAL_WIDTH / 2 - digdugwidth, 250 + digdugheight, 0, sx, sy)
     push:finish()
+end
+
+function love.keypressed(key)
+    if key == 'down' then
+        digdug.y  = digdug.y - 10
+    end
 end
--- src/constants.lua
+++ src/constants.lua
@@ -8,11 +8,20 @@
     cogden@cs50.harvard.edu
 
     Some global constants for our application.
-]]
-
-WINDOW_WIDTH = 600
+]] WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 650
 
+
+
+GRID = {} -- Main table
+
+-- Initialize the 2D array with zeros
+for i = 1, 30, 1 do
+    GRID[i] = {} -- Create a new row table
+    for j = 1, 30, 1 do
+        GRID[i][j] = 0
+    end
+end
 -- size we're trying to emulate with push
 VIRTUAL_WIDTH = 600
 VIRTUAL_HEIGHT = 650
